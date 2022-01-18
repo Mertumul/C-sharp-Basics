ce194401bbf5502ad8c08b7e33c4a32fbd4f0aad
Mert Umul <84383448+Mertumul@users.noreply.github.com>
2022-01-18 16:12:18 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 7;
+            switch (number)
+            {
+                case 10:
+                    Console.WriteLine("number is 10");
+                    break;
+                case 20:
+                    Console.WriteLine("number is 20");
+                    break;
+                default:
+                    Console.WriteLine("number is not 10 or 20");
+                    break;
+
+
+                
+            }
+
+
+        }
+    }
+}
