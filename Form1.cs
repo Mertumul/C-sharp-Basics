56e25dfd90da6ecb2262bd8297e915d7db05f750
Mert Umul <84383448+Mertumul@users.noreply.github.com>
2022-02-01 13:56:46 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tool_Usage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Mert Umul!");
+
+         
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Mert Umul!!";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label4.Text = "Mert";
+            label6.Text = "Umul";
+            label8.Text = "Computer Engineer";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //   textBox1.Text = "Computer Engineering";
+            label9.Text = textBox1.Text;
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
