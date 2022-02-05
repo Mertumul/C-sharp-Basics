84e84c0d6d17b981fe7b632a24a8e9285ff3e8a6
Mert Umul <84383448+Mertumul@users.noreply.github.com>
2022-02-05 12:25:31 +0300

    Add files via upload

---
+++ Basic shopping discount calculator/Basic shopping discount calculator/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace Basic_shopping_discount_calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblamount = new System.Windows.Forms.Label();
+            this.lbltotal = new System.Windows.Forms.Label();
+            this.lblprice = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(469, 329);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblamount
+            // 
+            this.lblamount.AutoSize = true;
+            this.lblamount.Location = new System.Drawing.Point(388, 224);
+            this.lblamount.Name = "lblamount";
+            this.lblamount.Size = new System.Drawing.Size(128, 18);
+            this.lblamount.TabIndex = 1;
+            this.lblamount.Text = "Amount of Books";
+            // 
+            // lbltotal
+            // 
+            this.lbltotal.AutoSize = true;
+            this.lbltotal.Location = new System.Drawing.Point(466, 278);
+            this.lbltotal.Name = "lbltotal";
+            this.lbltotal.Size = new System.Drawing.Size(50, 18);
+            this.lbltotal.TabIndex = 2;
+            this.lbltotal.Text = "Total:";
+            // 
+            // lblprice
+            // 
+            this.lblprice.AutoSize = true;
+            this.lblprice.Location = new System.Drawing.Point(519, 278);
+            this.lblprice.Name = "lblprice";
+            this.lblprice.Size = new System.Drawing.Size(49, 18);
+            this.lblprice.TabIndex = 3;
+            this.lblprice.Text = "00TL";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(522, 224);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 26);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(343, 7);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(471, 18);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "If you buy 0-20 between books your discounts wil bee 20 percent";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(343, 56);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(474, 18);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "If you buy 21-40between books your discounts wil bee 40 percent";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(343, 96);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(469, 18);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "If you buy greater than 41 books your discount will be 50 percent\r\n";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(453, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 18);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Each books equal to 10 TL";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(1200, 623);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblprice);
+            this.Controls.Add(this.lbltotal);
+            this.Controls.Add(this.lblamount);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Calculate";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblamount;
+        private System.Windows.Forms.Label lbltotal;
+        private System.Windows.Forms.Label lblprice;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Basic shopping discount calculator/Basic shopping discount calculator/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Basic_shopping_discount_calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int amountofbook, total;
+            amountofbook = Convert.ToUInt16(textBox1.Text);
+
+            if (amountofbook <= 20&&amountofbook>0)
+            {
+                total = amountofbook * 10 * 8 / 10;
+                lblprice.Text = total.ToString();
+
+
+            }
+            else if (amountofbook > 20 && amountofbook < 40)
+            {
+                total = amountofbook * 10 * 6 / 10;
+                lblprice.Text = total.ToString();
+
+
+            }
+            else if (amountofbook>50)
+            {
+                total = amountofbook * 10 * 5 / 10;
+                lblprice.Text= total.ToString();
+
+            }
+            else
+            {
+                lblprice.Text = "you have to enter greater than the 0 number ";
+            }
+        }
+    }
+}
