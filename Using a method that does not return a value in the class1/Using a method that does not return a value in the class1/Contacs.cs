59d9d02c3e3290048e172e2bc082b3f1eb22b4c4
Mert Umul <84383448+Mertumul@users.noreply.github.com>
2022-02-17 22:32:50 +0300

    Add files via upload

---
+++ Using a method that does not return a value in the class1/Using a method that does not return a value in the class1/Contacs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Using_a_method_that_does_not_return_a_value_in_the_class1
+{
+    class Contacs
+    {
+        public void contactslistandstatus(string name,string surname,string job)
+        {
+            Console.WriteLine("Name:" + name);
+            Console.WriteLine("Surname:" + surname);
+            Console.WriteLine("Job:" + job);
+
+
+
+        }
+    }
+}
---
+++ Using a method that does not return a value in the class1/Using a method that does not return a value in the class1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Using_a_method_that_does_not_return_a_value_in_the_class1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string surname;
+            string job;
+
+            Console.WriteLine("Enter Name:");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter Surname:");
+            surname = Console.ReadLine();
+            Console.WriteLine("Enter Job:");
+            job = Console.ReadLine();
+            Contacs c1 = new Contacs();
+            c1.contactslistandstatus("Mert","Umul","Computer Engineer");
+
+
+        }
+    }
+}
