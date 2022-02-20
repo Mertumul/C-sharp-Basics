7496be34b66959e32d5fadfb01abf2c09c58fa93
Mert Umul <84383448+Mertumul@users.noreply.github.com>
2022-02-20 22:47:41 +0300

    Add files via upload

---
+++ Chart(Graphic)ToolsUsage/Chart(Graphic)ToolsUsage/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chart_Graphic_ToolsUsage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            chart1.Series["Books"].Points.AddXY("Medetterenian", 5);
+            chart1.Series["Books"].Points.AddXY("Aegean", 8);
+            chart1.Series["Books"].Points.AddXY("Central Anatolia", 3);
+            chart1.Series["Books"].Points.AddXY("Marmara", 10);
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
