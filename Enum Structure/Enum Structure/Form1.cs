fc4c254bd19216155d083a13ee1de55c8d074d13
Mert Umul <84383448+Mertumul@users.noreply.github.com>
2022-02-22 21:10:39 +0300

    Add files via upload

---
+++ Enum Structure/Enum Structure/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Enum_Structure
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        enum cities {ADANA,ADIYAMAN,AFYON,AGRI,AMASYA,ANKARA,ANTALYA,ARTVİN,AYDIN}
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int number = Convert.ToInt16(textBox1.Text);
+            cities ci;
+            ci = (cities)number;
+            label1.Text = ci.ToString();
+        }
+    }
+}
