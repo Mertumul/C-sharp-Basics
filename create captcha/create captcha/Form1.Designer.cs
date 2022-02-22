d494f8bab430e10617d9cf0f804db84cef176e70
Mert Umul <84383448+Mertumul@users.noreply.github.com>
2022-02-22 11:45:18 +0300

    Add files via upload

---
+++ create captcha/create captcha/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace create_captcha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(225, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(131, 201);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(187, 49);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Random";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(131, 141);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(301, 54);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(324, 201);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 49);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Try";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(20F, 48F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(698, 436);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe Script", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(10, 11, 10, 11);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ create captcha/create captcha/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace create_captcha
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.BackColor = Color.White;
+            string[] symbol1 = { "A", "B", "C", "D", "E", "F", "G", "H" };
+            string[] symbol2 = { "e", "f", "g", "j", "z", "k", "m", "t" };
+            string[] symbol3 = {"+","*","-","/","#","$","&"};
+
+            Random rnd = new Random();
+            int s1, s2, s3, s4, s5;
+
+            s1 = rnd.Next(0, symbol1.Length);
+            s2 = rnd.Next(0, symbol2.Length);
+            s3 = rnd.Next(0, symbol3.Length);
+            s4 = rnd.Next(1, 5);
+            s5 = rnd.Next(5, 11);
+            label1.Text = symbol1[s1].ToString() + s4.ToString() + symbol3[s3].ToString() + s5.ToString() + symbol2[s2].ToString();
+
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == label1.Text)
+            {
+                label1.Text = "Login Succesfull";
+                label1.BackColor = Color.Green;
+            }
+            else
+            {
+                label1.Text = "Login Failed";
+                label1.BackColor = Color.Red;
+
+            }
+            textBox1.Text = "";
+        }
+    }
+}
