c3e9932e234c3dce23e6c7373ae08518e5625f23
Mert Umul <84383448+Mertumul@users.noreply.github.com>
2022-02-23 20:19:37 +0300

    Add files via upload

---
+++ Math Operators/Math Operators/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Math_Operators
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double number1;
+            number1 = Convert.ToDouble(textBox1.Text);
+            // label2.Text = Math.Abs(number1).ToString();
+            //label2.Text = Math.Ceiling(number1).ToString();
+            // label2.Text = Math.Floor(number1).ToString();
+            // label2.Text = Math.Pow(number1,3).ToString();
+            label2.Text = Math.Sqrt(number1).ToString();
+        }
+    }
+}
