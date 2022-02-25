aaaf7ad150b1902fe5bd92f57d03cb55580809c8
Mert Umul <84383448+Mertumul@users.noreply.github.com>
2022-02-25 16:31:25 +0300

    Add files via upload

---
+++ Multidimensial aaray example/Multidimensial aaray example/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Multidimensial_aaray_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matris = new int[4, 4];
+            int i, j;
+
+            for ( i = 0; i < 4; i++)
+            {
+                for (
+                    j = 0; j < 4; j++)
+                {
+                    Console.WriteLine("Matris[" + i + " " + j + "]=");
+                    string x = Console.ReadLine();
+                    matris[i, j] = System.Convert.ToInt16(x);
+                }
+            }
+        }
+    }
+}
