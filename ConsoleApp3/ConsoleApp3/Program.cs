f7e13224ca1971418d5480ad9a2d4797d0eedfb3
Mert Umul <84383448+Mertumul@users.noreply.github.com>
2022-02-25 16:26:31 +0300

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 10, 12, 20, 22, 17, 22, 9, 3, 1, 2 };
+            Single ort;
+            int total = 0;
+            int max, min;
+            max = a[0];
+            min = a[0];
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (max <= a[i])
+                {
+                    max = a[i];
+                }
+                if (min >= a[i])
+                {
+                    min = a[i];
+                }
+                Console.WriteLine("a[" + i + "]=" + a[i]);
+                total += a[i];
+            }
+            ort = Convert.ToSingle(total) / 10;
+            Console.WriteLine("Avarage:" + ort);
+            Console.WriteLine("Min:" + min);
+            Console.WriteLine("Max:" + max);
+
+
+            Console.ReadKey();
+        }
+    }
+}
