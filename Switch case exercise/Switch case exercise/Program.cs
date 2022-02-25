350adeb9ef6aa5e5eb585729455c4278c83c1630
Mert Umul <84383448+Mertumul@users.noreply.github.com>
2022-02-25 14:57:23 +0300

    Add files via upload

---
+++ Switch case exercise/Switch case exercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Switch_case_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 1; x < 6; x++)
+            {
+                switch (x)
+                {
+                    case 2:
+                        Console.WriteLine("x is {0}--in case2", x); break;
+                    case 5:
+                        Console.WriteLine("x is {0}--in case5", x); break;
+                    default:
+                        Console.WriteLine("x is {0}--in default case", x); break;
+
+
+                }
+            }
+
+        }
+    }
+}
