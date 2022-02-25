d65d4cf710f15bac0d6a6946a11c134adca760a7
Mert Umul <84383448+Mertumul@users.noreply.github.com>
2022-02-25 15:51:56 +0300

    Add files via upload

---
+++ loops exercise/loops exercise/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace loops_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, i, factor;
+            bool isprime;
+            for ( num = 2; num < 20; num++)
+            {
+                isprime = true;
+                factor = 0;
+                for ( i = 2; i < num/2; i++)
+                {
+                    if ((num % i) == 0)
+                    {
+                        isprime = false;
+                        factor = i;
+                    }
+                }
+                if (isprime)
+                    Console.WriteLine(num + "is prime.");
+                else
+                    Console.WriteLine(num + "is not prime.");
+
+            }
+        }
+    }
+}
