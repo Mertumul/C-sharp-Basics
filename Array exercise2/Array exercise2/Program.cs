b91bfb3d8fb376c134854210648eed8c7657023f
Mert Umul <84383448+Mertumul@users.noreply.github.com>
2022-03-04 15:00:42 +0300

    Add files via upload

---
+++ Array exercise2/Array exercise2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Array_exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] agac = new int[3][];
+            agac[0] = new int[3];
+            agac[1] = new int[5];
+            agac[2] = new int[2];
+            int i ,j;
+            for ( i = 0; i < 3; i++)
+            {
+                for ( j = 0; j < agac[i].Length; j++)
+                {
+                    Console.WriteLine("agac[" + i + "][" + j + "]=");
+                    string x = Console.ReadLine();
+                    agac[i][j] = System.Convert.ToInt32(x);
+                }
+
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
