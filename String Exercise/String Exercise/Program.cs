8dd4e410d50310cb1ea397c4ec2de0a85c45f8e2
Mert Umul <84383448+Mertumul@users.noreply.github.com>
2022-03-04 15:22:42 +0300

    Add files via upload

---
+++ String Exercise/String Exercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace String_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "This is a string example";
+            Console.WriteLine("message={0}", message);
+            Console.WriteLine("message.length={0}", message.Length);
+            for (int i = 0; i < message.Length; i++)
+            {
+                Console.WriteLine("message={0}={1}",i, message[i]);
+
+            }
+            Console.Read();
+
+        }
+    }
+}
