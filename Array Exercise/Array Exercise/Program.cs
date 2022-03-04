e955769d8ef741be3063fd44215a268ae1f72e77
Mert Umul <84383448+Mertumul@users.noreply.github.com>
2022-03-04 14:45:41 +0300

    Add files via upload

---
+++ Array Exercise/Array Exercise/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Array_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] list = { 10, 20, 25, 30, 15, 40 };
+            Console.WriteLine("Array is writing");
+            for (int i = 0; i < list.Length; i++)
+            {
+                Console.WriteLine(list[i]+" ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("5 değerinin indisi:"+Array.IndexOf(list,5));
+            list.SetValue(10, 5);
+
+            Console.WriteLine("Indis 3 deg;"+list.GetValue(3));
+            Console.WriteLine("Array is writing");
+
+            for (int i = 0; i < list.Length; i++)
+            {
+                Console.WriteLine(list[i] + " ");
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("15 değerinin indisi:" + Array.IndexOf(list, 15));
+
+
+
+
+        }
+    }
+}
